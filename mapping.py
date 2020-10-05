9e06aa71f53ea08ca1d904c439e191c30abefdde
dmathieu <42@dmathieu.com>
2020-10-05 09:45:26 +0200

    disable dynamic mapping

--- mapping.py
+++ mapping.py
@@ -7,6 +7,7 @@ class Mapping:
     def json(self):
         return {
             "mappings": {
+                "dynamic": False,
                 "properties": self.__properties()
             }
         }
--- test_mapping.py
+++ test_mapping.py
@@ -2,16 +2,16 @@ from mapping import Mapping
 
 def test_json_mapping():
     mapping = Mapping({"hello": "world"})
-    assert mapping.json() == {"mappings": {"properties": {"hello": {"type": "text"}}}}
+    assert mapping.json() == {"mappings": {"dynamic": False, "properties": {"hello": {"type": "text"}}}}
 
 def test_date_mapping():
     mapping = Mapping({"day": "2020-01-01"})
-    assert mapping.json() == {"mappings": {"properties": {"day": {"type": "date"}}}}
+    assert mapping.json() == {"mappings": {"dynamic": False, "properties": {"day": {"type": "date"}}}}
 
 def test_integer_mapping():
     mapping = Mapping({"value": "25"})
-    assert mapping.json() == {"mappings": {"properties": {"value": {"type": "double"}}}}
+    assert mapping.json() == {"mappings": {"dynamic": False, "properties": {"value": {"type": "double"}}}}
 
 def test_dep_mapping():
     mapping = Mapping({"dep": "25"})
-    assert mapping.json() == {"mappings": {"properties": {"dep": {"type": "text"}}}}
+    assert mapping.json() == {"mappings": {"dynamic": False, "properties": {"dep": {"type": "text"}}}}
